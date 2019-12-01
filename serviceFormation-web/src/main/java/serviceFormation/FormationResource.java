fa750df6fa8d3adc2b76ecb1ab45a259713dc762
Chambriard <alban.chambriard@gmail.com>
2019-12-01 15:29:41 +0100

    bonjour claire

---
+++ serviceFormation-web/src/main/java/serviceFormation/FormationResource.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serviceFormation;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author alban
+ */
+@Path("formation")
+@RequestScoped
+public class FormationResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of FormationResource
+     */
+    public FormationResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of serviceFormation.FormationResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public String getXml() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of FormationResource
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_XML)
+    public void putXml(String content) {
+    }
+}
